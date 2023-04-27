c4a38ac85c56a568ddde68c22fad4eb6516b8388
MasubaCoder10 <124436859+MasubaCoder10@users.noreply.github.com>
2023-04-27 09:28:30 +0100

    Add files via upload

---
+++ CssGrid/app.js
@@ -0,0 +1,20 @@
+let userOne = {
+    name: 'Asabeneh',
+    role: 'teaching',
+    country: 'Finland'
+}
+
+let userTwo = {
+    name: 'Asabeneh',
+    role: 'teaching',
+    country: 'Finland'
+
+}
+
+let nums = [1, 2, 3]
+let numbers = nums
+
+console.log(nums == numbers)
+console.log(`${numbers}`)
+let pi = Math.PI
+console.log(`${pi}`)
